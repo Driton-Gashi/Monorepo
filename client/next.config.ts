0a7a2a0f9cdd9d5a4e457d005eeba09b7b6b2d87
Driton Gashi <dritongashi1995@gmail.com>
2025-03-25 12:58:54 +0100

    fix: trying to fix /dashboard/food/[id] page not being found

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,18 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: process.env.NEXT_PUBLIC_GATEWAY_URL ?? '',
-          port: '',
-          pathname: '/ipfs/**',
-          search: '',
-        },
-      ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_GATEWAY_URL ?? '',
+        pathname: '/ipfs/**',
+      },
+    ],
+  },
+  skipTrailingSlashRedirect: true,
+  output: process.env.NEXT_OUTPUT_MODE === 'export' ? 'export' : undefined,
 };
 
 export default nextConfig;
